4fc3c22e3d25c230050203f914bb8b308e96ac94
Jack Deng <jack.z.deng@outlook.com>
2023-11-20 10:05:45 +1030

    update: tree

---
+++ beginner/binary_tree.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self, data=None):
+        if data:
+            self.root = Node(data)
+        else:
+            self.root = None
+
+    def insert(self, data):
+        if self.root:
+            if data < self.root.data:
+                if self.root.left:
+                    self.root.left.insert(data)
+                else:
+                    self.root.left = BinaryTree(data)
+            else:
+                if self.root.right:
+                    self.root.right.insert(data)
+                else:
+                    self.root.right = BinaryTree(data)
+        else:
+            self.root = Node(data)
+
+    def inorder(self):
+        if self.root is None:
+            return
+        if self.root.left is not None:
+            self.root.left.inorder()
+        print(self.root.data)
+        if self.root.right is not None:
+            self.root.right.inorder()
+
+    def preorder(self):
+        if self.root is None:
+            return
+        print(self.root.data)
+        if self.root.left is not None:
+            self.root.left.preorder()
+        if self.root.right is not None:
+            self.root.right.preorder()
+
+    def postorder(self):
+        if self.root is None:
+            return
+        if self.root.left is not None:
+            self.root.left.postorder()
+        if self.root.right is not None:
+            self.root.right.postorder()
+        print(self.root.data)
+
+    # def deleteNode(self, data):
+    #     if self.root:
+    #         if data < self.root.data:
+    #             self.root.left =
+    #     return self.root
+
+
+if __name__ == "__main__":
+    tree = BinaryTree()
+    tree.insert(50)
+    tree.insert(30)
+    tree.insert(20)
+    tree.insert(40)
+    tree.insert(70)
+    tree.insert(60)
+    tree.insert(80)
+
+    print("Inorder traversal of the original tree:")
+    tree.inorder()
+
+    print("\nPreorder traversal of the original tree:")
+    tree.preorder()
+
+    print("\nPostorder traversal of the original tree:")
+    tree.postorder()
